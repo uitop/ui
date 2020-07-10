773e9563dec8ff3e9e2e8c25ae0d5f3d8383171c
JSPARK\JSP <dev.grayt@gmail.com>
2020-07-10 18:57:29 +0900

    2020 업데이트

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,8 +10,8 @@ app.controller('myPof', function($scope){
     }
 });
 app.controller("radarCht", function ($scope) {
-    $scope.labels = ["Communication","Frontend","UX/UI","Tool","Search","Health"];
-    $scope.data = [[100,95, 90, 90, 95,100]] ;
+    $scope.labels = ["체력","지구력","실력","이해력","매력","적응력"];
+    $scope.data = [[95,95, 95, 95, 90,100]] ;
     $scope.options = {
         elements:{
             point:{
@@ -69,18 +69,20 @@ app.controller('SlickController', function ($scope) {
         ]
     };
     $scope.links = [
-        {'a':'http://www.yzpark.kr/','t':'와이즈파크','y':2014,'i':'워드프레스 기반 블로그'},
-        {'a':'http://homepage.kim/','t':'김씨네 홈페이지','y':2015,'i':'직접만드는 홈페이지, 웹에디터 서비스'},
-        {'a':'http://thesupkorea.com/home.php','t':'더숲 코리아','y':2016,'i':'비동기식 갤러리, 회사소개 사이트'},
-        {'a':'http://coffeecave.co.kr/','t':'커피케이브','y':2016,'i':'고도몰 기반 커피용품 쇼핑몰'},
-        {'a':'https://www.thehigh1.com/','t':'더하이원펀딩','y':2017,'i':'크라우드펀딩 사이트'},
-        {'a':'https://www.daily-funding.com/','t':'데일리펀딩','y':2017,'i':'크라우드펀딩 사이트'},
-        {'a':'https://www.finstreet.co.kr/','t':'핀스트리트','y':2017,'i':'크라우드펀딩 사이트'},
-        {'a':'assets/img/po07.png','t':'어학원 사이트','y':2017,'i':'이미지 페이지로 이동합니다.'},
-        {'a':'http://www.winnersplan.co.kr/','t':'위너스플랜','y':2017,'i':'그누보드 기반 회사소개 사이트'},
-        {'a':'http://www.bullsinvest.co.kr/','t':'불스인베스트','y':2017,'i':'주식정보 제공사이트'},
-        {'a':'assets/img/po010.png','t':'가상화페 거래소 지갑화면','y':2018,'i':'이미지 페이지로 이동합니다.'},
-        {'a':'http://event.pping.kr','t':'가격비교 사이트 이벤트 리스트','y':2018,'i':'행복쇼핑 이벤트'}
+        {n:0,a:'http://www.yzpark.kr/',t:'와이즈파크',y:2014,i:'워드프레스 기반 블로그'},
+        {n:1,a:'http://homepage.kim/',t:'김씨네 홈페이지',y:2015,i:'직접만드는 홈페이지, 웹에디터 서비스'},
+        {n:2,a:'http://thesupkorea.com/home.php',t:'더숲 코리아',y:2016,i:'비동기식 갤러리, 회사소개 사이트'},
+        {n:3,a:'http://coffeecave.co.kr/',t:'커피케이브',y:2016,i:'고도몰 기반 커피용품 쇼핑몰'},
+        {n:4,a:'https://www.thehigh1.com/',t:'더하이원펀딩',y:2017,i:'크라우드펀딩 사이트'},
+        {n:5,a:'https://www.daily-funding.com/',t:'데일리펀딩',y:2017,i:'크라우드펀딩 사이트'},
+        {n:6,a:'https://www.finstreet.co.kr/',t:'핀스트리트',y:2017,i:'크라우드펀딩 사이트'},
+        {n:7,a:'assets/img/po7.png',t:'어학원 사이트',y:2017,i:'이미지 페이지로 이동합니다.'},
+        {n:8,a:'http://www.winnersplan.co.kr/',t:'위너스플랜',y:2017,i:'그누보드 기반 회사소개 사이트'},
+        {n:9,a:'http://www.bullsinvest.co.kr/',t:'불스인베스트',y:2017,i:'주식정보 제공사이트'},
+        {n:10,a:'assets/img/po10.png',t:'가상화페 거래소 지갑화면',y:2018,i:'이미지 페이지로 이동합니다.'},
+        {n:11,a:'http://event.pping.kr',t:'행복쇼핑 이벤트',y:2018,i:'가격비교 사이트 이벤트 리스트'},
+        {n:12,a:'http://www.innerinfo.net/',t:'이너인포',y:2019,i:'회사소개 사이트'},
+        {n:13,a:'https://www.kaida.co.kr/',t:'KAIDA',y:2020,i:'수입차 정보제공 사이트'}
     ]
 });
 app.controller('footerArea', function($scope){
